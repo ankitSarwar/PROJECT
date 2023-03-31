d523b2e89b040eec73981d6ec187a86ae6a130c9
tutorials-Git <ankitsarwar440@gmail.com>
2023-03-31 12:09:35 +0530

    project

--- Tic_Tac_Toe/Board.java
+++ Tic_Tac_Toe/Board.java
@@ -17,17 +17,17 @@ public class Board {
     public String toString() { //linear
         StringBuilder sb = new StringBuilder();
 
-         for(char[] oned : this.board) {
-             for(char c : oned) sb.append(c).append("\t");
-             sb.append("\n");
-         }
+//         for(char[] oned : this.board) {
+//             for(char c : oned) sb.append(c).append("\t");
+//             sb.append("\n");
+//         }
 
-//        for(int i = 0; i < this.size; i ++) {
-//            for(int j = 0; j < this.size; j ++) {
-//                sb.append(this.board[i][j]).append("\t");
-//            }
-//            sb.append("\n");
-//        }
+        for(int i = 0; i < this.size; i ++) {
+            for(int j = 0; j < this.size; j ++) {
+                sb.append(this.board[i][j]).append("\t");
+            }
+            sb.append("\n");
+        }
         return sb.toString();
     }
 
